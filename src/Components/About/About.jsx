b52ca7c45e995cdc26bf7eab68fbaed2f0ff78f3
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-09-28 23:17:24 +0300

    edit responsive #2

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,12 +3,13 @@ import cloudChange from "../../assets/cloud-change.svg";
 import about from "../../assets/about1.svg";
 import about2 from "../../assets/about2.svg";
 import { Link } from "react-router-dom";
+import "./About.css";
 
 const About = () => {
   return (
     <section className="px-20 about mt-5" id="aboutSection">
       <div className="container space-y-5">
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap about-content">
           {/* Left Section */}
           <div className="w-full sm:w-1/3 p-3">
             <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
@@ -29,55 +30,62 @@ const About = () => {
           </div>
 
           {/* Right Section */}
-          <div className="w-full sm:w-2/3 p-3 px-3 mt-0 flex flex-wrap justify-around text-center">
+
+          <div className=" h-[240px] w-full sm:w-2/3 p-3 px-3 mt-0 flex flex-wrap justify-around text-center right-section">
             {/* First Box */}
-            <div className="lg:w-1/3 w-full shadow-md p-2 bg-white rounded-md relative lg:mb-0 box">
-              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
-                <img src={cloudConnection} alt="Cloud Connection" />
-              </div>
-              <h4 className="text-lg font-bold mt-8">ERP System</h4>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur
-              </p>
-              <div>
-                <Link
-                  className="inline-block bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-                  to="/erp"
-                >
-                  Subscribe now
-                </Link>
+            <div className="flex flex-col justify-end">
+              <div className="w-[250px] shadow p-2 pb-4 bg-white rounded-md relative lg:mb-0 box order-2 lg:order-1 ">
+                <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
+                  <img src={cloudConnection} alt="Cloud Connection" />
+                </div>
+                <h4 className="text-lg font-bold mt-8">ERP System</h4>
+                <p className="text-gray-400">
+                  Lorem ipsum dolor sit amet, consectetur
+                </p>
+                <div>
+                  <Link
+                    className="bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out absolute -bottom-9 left-10 "
+                    to="/erp"
+                  >
+                    Subscribe now
+                  </Link>
+                </div>
               </div>
             </div>
 
-            {/* Second Box */}
-            <div className="lg:w-1/3 w-full shadow-md p-2 bg-white rounded-md relative lg:mb-0 box">
-              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
-                <img src={cloudChange} alt="Cloud Change" />
-              </div>
-              <h4 className="text-lg font-bold mt-8">Data Transfer</h4>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur
-              </p>
-              <div>
-                <button className="inline-block bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Subscribe now
-                </button>
-              </div>
-            </div>
+            {/* Second Box (Middle Box) */}
+           <div className="middleBox">
+             <div className="w-[250px] shadow p-2 pb-4 bg-white rounded-md relative lg:mb-0 box mb-4">
+               <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
+                 <img src={cloudChange} alt="Cloud Change" />
+               </div>
+               <h4 className="text-lg font-bold mt-8">Data Transfer</h4>
+               <p className="text-gray-400">
+                 Lorem ipsum dolor sit amet, consectetur
+               </p>
+               <div>
+                 <button className="inline-block bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out absolute -bottom-9 left-10 ">
+                   Subscribe now
+                 </button>
+               </div>
+             </div>
+           </div>
 
             {/* Third Box */}
-            <div className="lg:w-1/3 w-full shadow-md p-2 bg-white rounded-md relative mb-16 lg:mb-0 box">
-              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
-                <img src={cloudConnection} alt="Cloud Connection" />
-              </div>
-              <h4 className="text-lg font-bold mt-8">Email and Hosting</h4>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur
-              </p>
-              <div>
-                <button className="inline-block bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Subscribe now
-                </button>
+            <div className="flex flex-col justify-end lastBox">
+              <div className="w-[250px] shadow-md p-2 pb-4 bg-white rounded-md relative mb-16 lg:mb-0 box order-3 lg:order-3">
+                <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-md bg-main p-2">
+                  <img src={cloudConnection} alt="Cloud Connection" />
+                </div>
+                <h4 className="text-lg font-bold mt-8">Email and Hosting</h4>
+                <p className="text-gray-400">
+                  Lorem ipsum dolor sit amet, consectetur
+                </p>
+                <div>
+                  <button className="inline-block bg-main text-white lg:mx-0 font-bold rounded-md my-3 py-2 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out absolute -bottom-9 left-10 ">
+                    Subscribe now
+                  </button>
+                </div>
               </div>
             </div>
           </div>
