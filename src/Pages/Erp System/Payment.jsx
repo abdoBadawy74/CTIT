12545d4cc3e39f26d130caf3b7f16ef8dad150a7
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-10-07 07:51:44 +0300

    prepare payment until solve server error

--- src/Pages/Erp System/Payment.jsx
+++ src/Pages/Erp System/Payment.jsx
@@ -1,116 +1,213 @@
-import React, { useEffect } from "react";
-// translate
+import React, { useState } from "react";
 import useLanguage from "../../Context/useLanguage";
 import t from "../../translation/translation";
 import QRCode from "../../assets/qr-code-img.png";
 import { Link } from "react-router-dom";
+import PDF from "../../assets/pdf_icon.svg";
 import axios from "axios";
 
 export default function Payment() {
-  // translate
   const { language } = useLanguage();
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("upload");
+  const [filePreview, setFilePreview] = useState(null);
+  const [fileType, setFileType] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null); // Track the actual file
 
-  useEffect(() => {
-    axios
-      .post("https://aldaifii.ctit.com.sa/saas/available_online_payment", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const handlePaymentMethodChange = (event) => {
+    setSelectedPaymentMethod(event.target.value);
+  };
+
+  const onFileInputChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+      const reader = new FileReader();
+      setSelectedFile(file); // Store the actual file for submission
+
+      if (fileExtension === "pdf") {
+        setFileType("pdf");
+        setFilePreview(URL.createObjectURL(file));
+      } else if (["png", "jpg", "jpeg", "gif"].includes(fileExtension)) {
+        setFileType("image");
+        reader.onload = () => {
+          setFilePreview(reader.result);
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert("Unsupported file type! Please select an image or a PDF.");
+        setFilePreview(null);
+      }
+    }
+  };
+
+  const handleRemoveFile = () => {
+    setFilePreview(null);
+    setFileType(null);
+    setSelectedFile(null); // Clear the file
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create form data
+    const formData = new FormData();
+    formData.append("pre_subscription_id", 5); // static value
+    formData.append("attachment", selectedFile || ""); // include file if present
+    console.log(selectedFile);
+    try {
+      const response = await axios.post(
+        "https://aldaifii.ctit.com.sa/saas/set_payment_attachment",
+        {
+          params: {
+            pre_subscription_id: 55, // static value
+            attachment: selectedFile, // send attachment if file selected
+          },
+        }
+      );
+
+      // Handle response (success)
+      console.log("Form submitted successfully:", response.data);
+    } catch (error) {
+      // Handle error
+      console.error("Error submitting form:", error);
+    }
+  };
 
   return (
     <div className="bg-[#F8F9F9] pt-15 pb-15 payment">
-      <div className="flex items-center justify-center lg:justify-between mb-8 px-20 pt-5  gap-5 flex-wrap-reverse">
-        <form className="flex self-start gap-5 flex-col">
-          <div className="flex items-center gap-1 md:gap-3 justify-center sm:justify-start ">
-            {/* Radio Buttons for Category Selection */}
-            <div className="flex items-center ">
-              <input type="radio" id="upload" value="upload" />
+      <div className="flex items-center justify-center lg:justify-between mb-8 px-20 pt-5 gap-5 flex-wrap-reverse">
+        <form
+          className="flex self-start gap-5 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex items-center gap-1 md:gap-3 justify-center sm:justify-start">
+            <div className="flex items-center">
+              {" "}
+              <input
+                type="radio"
+                id="upload"
+                value="upload"
+                checked={selectedPaymentMethod === "upload"}
+                onChange={handlePaymentMethodChange}
+              />
               <label htmlFor="upload" className="ml-2">
-                {t[language].Uplaod}
+                {t[language].Upload}
               </label>
             </div>
             <div className="flex items-center">
-              <input type="radio" id="online" value="online" />
+              <input
+                type="radio"
+                id="online"
+                value="online"
+                checked={selectedPaymentMethod === "online"}
+                onChange={handlePaymentMethodChange}
+              />
               <label htmlFor="online" className="ml-2">
                 {t[language].Online}
               </label>
             </div>
-            <Link
-              to="/login"
-              type="button"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm md:text-base"
-            >
-              {t[language].Back}
-            </Link>
+            {!localStorage.getItem("LoginEmail") && (
+              <Link
+                to="/login"
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm md:text-base"
+              >
+                {t[language].Back}
+              </Link>
+            )}
           </div>
 
-          {/* Upload Receipt Section */}
-          <label
-            htmlFor="dropzone-file"
-            className="relative flex flex-col items-center justify-center w-[350px] md:w-[450px] h-[300px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
-          >
-            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                className="w-8 h-8 mb-4 text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 16"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p className="mb-2 text-sm text-gray-500">
-                <span className="font-semibold">{t[language].Click}</span>{" "}
-                {t[language].Or}
-              </p>
-              <p className="text-xs text-gray-500">
-                SVG, PNG, JPG or GIF (MAX. 800x400px)
-              </p>
-            </div>
-            <input id="dropzone-file" type="file" className="hidden" />
-          </label>
+          {/* Conditionally render content based on the selected payment method */}
+          {selectedPaymentMethod === "upload" ? (
+            <>
+              {/* Conditionally render the label or file preview */}
+              {filePreview ? (
+                <div className="flex flex-col w-[350px] md:w-[450px] h-[300px] items-center justify-center">
+                  {fileType === "image" ? (
+                    <img
+                      src={filePreview}
+                      alt="Selected"
+                      className="h-[250px] w-50 object-cover mt-10 rounded"
+                    />
+                  ) : (
+                    <a
+                      href={filePreview}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 underline"
+                    >
+                      <img src={PDF} alt="PDF" className="w-20 h-20" />
+                      Preview PDF
+                    </a>
+                  )}
+                  <button
+                    onClick={handleRemoveFile}
+                    className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
+                  >
+                    Remove File
+                  </button>
+                </div>
+              ) : (
+                <label
+                  htmlFor="dropzone-file"
+                  className="relative flex flex-col items-center justify-center w-[350px] md:w-[450px] h-[300px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-gray-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500">
+                      <span className="font-semibold">{t[language].Click}</span>{" "}
+                      {t[language].Or}
+                    </p>
+                    <p className="text-xs text-gray-500">
+                      SVG, PNG, JPG or GIF (MAX. 800x400px)
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                    onChange={onFileInputChange}
+                  />
+                </label>
+              )}
 
-          {/* Confirm Button */}
-          <button
-            type="submit"
-            className="mt-4 bg-blue-500 w-full text-white py-2 px-4 rounded self-center"
-          >
-            {t[language].Confirm}
-          </button>
-
-          {/* Bank Details Section */}
-          <div className="flex items-center space-x-4 pt-4">
-            <img src={QRCode} alt="QR Code" className="w-20 h-20" />
-            <div className="flex items-center space-x-4">
-              <span
-                className="text-lg font-semibold text-gray-800"
-                style={{ fontSize: "1.5rem", color: "#002b54" }}
+              {/* Confirm Button */}
+              <button
+                type="submit"
+                className="mt-4 bg-blue-500 w-full text-white py-2 px-4 rounded self-center"
               >
-                {t[language].BankDetails}
-              </span>
-              <a
-                href="#"
-                className="text-blue-500 underline px-4"
-                style={{ fontSize: "1.5rem" }}
-              >
-                {t[language].View}
-              </a>
-            </div>
-          </div>
+                {t[language].Confirm}
+              </button>
+            </>
+          ) : (
+            <>
+              {/* Online Payment Section */}
+              <div className="flex flex-col items-center">
+                <p className="w-80 h-40 mb-4 text-center text-[20px] pt-10">
+                  Will Avialable Soon...
+                </p>
+                {/* Add your online payment form or buttons here */}
+                {/* <button className="mt-4 bg-blue-500 w-full text-white py-2 px-4 rounded">
+                  {t[language].Online}
+                </button> */}
+              </div>
+            </>
+          )}
         </form>
 
         {/* Promo Code Section */}
@@ -127,7 +224,7 @@ export default function Payment() {
                 placeholder={
                   language === "en" ? "Enter Promo Code" : "أدخل كود الخصم"
                 }
-                className="p-2 outline-none "
+                className="p-2 outline-none"
               />
               <button
                 type="button"
@@ -144,26 +241,15 @@ export default function Payment() {
               <span>500 $</span>
             </p>
             <p className="flex justify-between">
-              <span>{t[language].Subtotal}</span>
-              <span>500 $</span>
-            </p>
-            <p className="flex justify-between">
-              <span>{t[language].Subtotal}</span>
-              <span>500 $</span>
+              <span>{t[language].Discount}</span>
+              <span>- 0 $</span>
             </p>
           </div>
-          <div className="font-semibold">
-            <p className="flex justify-between">
-              <span>{t[language].Total}</span>
-              <span>500 $</span>
-            </p>
-          </div>
+          <p className="flex justify-between">
+            <span className="text-[#303B49]">{t[language].Total}</span>
+            <span className="text-[#303B49]">500 $</span>
+          </p>
         </div>
-      </div>
-
-      {/* Countdown Timer */}
-      <div className="flex justify-between text-gray-500 px-20">
-        <span>Countdown: 30 seconds</span>
       </div>
     </div>
   );
